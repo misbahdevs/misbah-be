8ae065bf1004efcdffab10f5e84a63473dfe4ef7
misbahdevs <misbahx.id@gmail.com>
2023-12-28 18:04:01 +0700

    feat(api): create register api

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,12 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Contact extends Model
 {
     protected $table = "contacts";
     protected $keyType = "int";
     protected $primaryKey = "id";
-    protected $incrementing = true;
-    protected $timestamps = true;
+    public $incrementing = true;
+    public $timestamps = true;
+
+    public function user():BelongsTo 
+    {
+        return $this->belongsTo(Contact::class, "user_id", "id");
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
     protected $table = "users";
     protected $primaryKey = "id";
     protected $keyType = "int";
-    protected $timestamps = true;
-    protected $incrementing = true;
+    public $timestamps = true;
+    public $incrementing = true;
 
+    protected $fillable = [
+        "username",
+        "password",
+        "name"
+    ];
+
+    public function contact(): HasMany 
+    {
+        return $this->hasMany(Contact::class, "user_id", "id");
+    }
 }
