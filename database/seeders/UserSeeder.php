f174f2880b4bf2dd3a2a5fdcfc3d9a4f95784cd8
misbahdevs <misbahx.id@gmail.com>
2023-12-29 06:29:42 +0700

    test(api): create unit test login api

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,10 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            "username" => "test",
+            "password" => Hash::make('test'),
+            "name" => "test"
+        ]);
     }
 }
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,7 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Database\Seeders\UserSeeder;
 use Tests\TestCase;
+
+use function PHPUnit\Framework\assertNotNull;
 
 class UserTest extends TestCase
 {
@@ -52,6 +56,52 @@ class UserTest extends TestCase
             "errors" => [
                 "username" => [
                     "username already registered."
+                ]
+            ]
+            ]);
+    }
+
+    public function testLoginSuccess()
+    {
+        $this->seed([UserSeeder::class]);
+        $this->post("/api/users/login", [
+            "username" => "test",
+            "password" => "test",
+        ])->assertStatus(200)->assertJson([
+            "data" => [
+                "username" => "test",
+                "name" => "test"
+            ]
+            ]);
+
+        $user = User::where('username', 'test')->first();
+        self::assertNotNull($user->token);
+    }
+
+    public function testLoginFailureUserNotFound()
+    {
+        $this->post("/api/users/login", [
+            "username" => "test",
+            "password" => "test",
+        ])->assertStatus(401)->assertJson([
+            "errors" => [
+                "message" => [
+                    "username or password wrong."
+                ]
+            ]
+            ]);
+    }
+
+    public function testLoginWrongPassword()
+    {
+        $this->seed([UserSeeder::class]);
+        $this->post("/api/users/login", [
+            "username" => "test",
+            "password" => "salah",
+        ])->assertStatus(401)->assertJson([
+            "errors" => [
+                "message" => [
+                    "username or password wrong."
                 ]
             ]
             ]);
