bb032015a3ab574b5b3020adb0cc8738174350ed
misbahdevs <misbahx.id@gmail.com>
2023-12-29 08:32:00 +0700

    feat(api): create update user api & unit test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserRegisterRequest;
+use App\Http\Requests\UserUpdateRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
@@ -51,6 +55,30 @@ class UserController extends Controller
         }
 
         $user->token = Str::uuid()->toString();
+        $user->save();
+
+        return new UserResource($user);
+    }
+
+    public function get(Request $request): UserResource
+    {
+        $user = Auth::user();
+        return new UserResource($user);
+    }
+
+    public function update(UserUpdateRequest $request): UserResource 
+    {
+        $data = $request->validated();
+        $user = Auth::user();
+
+        if (isset($data['name'])) {
+            $user->name = $data['name'];
+        }
+
+        if (isset($data['password'])) {
+            $user->password = Hash::make($data['password']);
+        }
+
         $user->save();
 
         return new UserResource($user);
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -106,4 +106,58 @@ class UserTest extends TestCase
             ]
             ]);
     }
+
+    public function testUpdatePasswordSuccess()
+    {
+        $this->seed([UserSeeder::class]);
+
+        $oldUser = User::where("username", "test")->first();
+        $this->patch("/api/users/current",
+        ["password" => "new"],
+        ["Authorization" => "test"]
+        )->assertStatus(200)->assertJson([
+            "data" => [
+                "username" => "test",
+                "name" => "test"
+            ]
+            ]);
+
+        $newUser = User::where("username", "test")->first();
+        self::assertNotEquals($oldUser->password, $newUser->password);
+    }
+
+    public function testUpdateNameSuccess()
+    {
+        $this->seed([UserSeeder::class]);
+
+        $oldUser = User::where("username", "test")->first();
+        $this->patch("/api/users/current",
+        ["name" => "Misbah"],
+        ["Authorization" => "test"]
+        )->assertStatus(200)->assertJson([
+            "data" => [
+                "username" => "test",
+                "name" => "Misbah"
+            ]
+            ]);
+
+        $newUser = User::where("username", "test")->first();
+        self::assertNotEquals($oldUser->name, $newUser->name);
+    }
+
+    public function testUpdateFailure()
+    {
+        $this->seed([UserSeeder::class]);
+
+        $this->patch("/api/users/current",
+        ["name" => "MisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestesMisbahtestestestestestestestestes"],
+        ["Authorization" => "test"]
+        )->assertStatus(400)->assertJson([
+            "errors" => [
+                "name" => [
+                    "The name field must not be greater than 100 characters."
+                ]
+            ]
+            ]);
+    }
 }
